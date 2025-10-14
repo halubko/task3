53ecf84913c77ab080e881bb9876e46351092471
halubko <misha.golubko@gmail.com>
2025-10-14 21:38:13 +0300

    fixed bug with pagination

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -3,23 +3,26 @@ import {productsAPI} from "../services/productsService";
 import {Box, Grid, Pagination, TablePagination} from "@mui/material";
 import ProductCard from "../components/product/ProductCard";
 import ProductPagination from "../components/product/ProductPagination";
+import ProductFilter from '../components/product/ProductFilter';
 
 const ProductsPage = () => {
     const [limit, setLimit] = useState<number>(10);
     const [skip, setSkip] = useState<number>(0);
     const {data} = productsAPI.useGetProductsQuery({limit, skip})
     const [page, setPage] = useState<number>(1);
-    const totalPages = data ? Math.ceil(data.total/data.limit) : 1
+    const totalPages = data ? Math.ceil(data.total/limit) : 1
+
 
     return (
-        <Box>
-            <Grid container spacing={1}>
-                {data && data.products.map(product =>
-                    <ProductCard key={product.id} product={product}/>
-                )}
-            </Grid>
-            <ProductPagination totalPages={totalPages} page={page} setPage={setPage} limit={limit} setLimit={setLimit} setSkip={setSkip}/>
-        </Box>
+            <Box sx={{flexGrow: 1}}>
+                <ProductFilter/>
+                    <Grid container spacing={1}>
+                        {data && data.products.map(product =>
+                            <ProductCard key={product.id} product={product}/>
+                        )}
+                    </Grid>
+                <ProductPagination totalPages={totalPages} page={page} setPage={setPage} limit={limit} setLimit={setLimit} setSkip={setSkip}/>
+            </Box>
     );
 };
 
