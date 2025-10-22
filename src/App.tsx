da0182a1c66f0eed0985f61d378837040d5efcdf
halubko <misha.golubko@gmail.com>
2025-10-22 22:57:55 +0300

    added checking access token and autoinit cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from "react"
+import { Outlet } from "react-router-dom"
+import { authAPI } from "./services/authService"
+import { useAppDispatch } from "./hooks/redux"
+import { setUser } from "./store/slices/authSlice"
+import { IUser } from "./models/IUser"
+import { cartAPI } from "./services/cartService"
+import { addCart } from "./store/slices/cartSlice"
 
 export function App() {
-    return (
-        <Outlet/>
-    );
+   const { data: userData } = authAPI.useCheckAuthQuery()
+
+   //Not working at DummyJSON at all, but logic is the next:
+   //When initializing the application, we check auth and automatically load the cart by userId
+   const { data: cartData } = cartAPI.useGetCartByUserIdQuery(userData?.id, { skip: !userData?.id })
+   const dispatch = useAppDispatch()
+
+   useEffect(() => {
+      if (userData) {
+         dispatch(
+            setUser({
+               user: { id: userData.id, username: userData.username } as IUser,
+               accessToken: localStorage.getItem("accessToken"),
+               refreshToken: localStorage.getItem("refreshToken"),
+            })
+         )
+      }
+
+      //DummyJSON do not allow to send empty array to server
+   }, [userData])
+
+   useEffect(() => {
+      if (cartData) {
+         console.log("cartData", cartData)
+         dispatch(addCart({ cartId: cartData.id, products: cartData.products }))
+      }
+   }, [cartData])
+
+   return <Outlet />
 }
