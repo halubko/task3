a83e09d2c7d65fac23237c448f0845eb54f10aff
halubko <misha.golubko@gmail.com>
2025-10-22 23:03:43 +0300

    cleare signup page for next refactor

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import {Box, Container} from "@mui/material";
-import AuthForm from "../components/AuthForm";
+import React from "react"
+import { Box, Container } from "@mui/material"
 
 const SignUpPage = () => {
-    return (
-        <Container component="main" sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}>
-            <Box sx={{ width: '100%' }}>
-                <AuthForm mode='signup'/>
-            </Box>
-        </Container>
-    );
-};
+   return (
+      <Container component="main" sx={{ height: "100vh", display: "flex", alignItems: "center" }}>
+         <Box sx={{ width: "100%" }}>SI</Box>
+      </Container>
+   )
+}
 
-export default SignUpPage;
+export default SignUpPage
