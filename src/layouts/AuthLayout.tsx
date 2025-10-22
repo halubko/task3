a6a3f6050d5ed6eec894f9a3d4841df8f8a4f131
halubko <misha.golubko@gmail.com>
2025-10-22 23:05:18 +0300

    added new mainlayout and fixed auth

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react"
+import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
-    return (
-        <div>
-            <Outlet/>
-        </div>
-    );
-};
+   return (
+      <div>
+         <Outlet />
+      </div>
+   )
+}
 
-export default AuthLayout;
+export default AuthLayout
