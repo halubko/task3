bee378a83edd653b20e4d295ae6b9230eaba7c01
halubko <misha.golubko@gmail.com>
2025-10-22 23:02:34 +0300

    fixed login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import {Box, CircularProgress, Container} from "@mui/material";
-import AuthForm from "../components/AuthForm";
-import {useAppSelector} from "../hooks/redux";
-import AuthError from "../components/AuthError";
+import React, { useEffect } from "react"
+import { Box, CircularProgress, Container } from "@mui/material"
+import AuthForm from "../components/auth/AuthForm"
+import AuthError from "../components/auth/AuthError"
+import { authAPI } from "../services/authService"
+import { useNavigate } from "react-router-dom"
+import { useAppDispatch } from "../hooks/redux"
+import { setUser } from "../store/slices/authSlice"
 
 const LoginPage = () => {
-    const {isLoading, error} = useAppSelector(state => state.user);
+   const [loginUser, { data, error, isLoading, isSuccess }] = authAPI.useLoginUserMutation()
+   const navigate = useNavigate()
+   const dispatch = useAppDispatch()
 
-    console.log(error)
+   useEffect(() => {
+      if (isSuccess) {
+         dispatch(
+            setUser({
+               user: { id: data.id, username: data.username },
+               accessToken: data.accessToken,
+               refreshToken: data.refreshToken,
+            })
+         )
+         void navigate("/main/products")
+      }
+   }, [isSuccess])
 
-    return (
-        <Container
-            component="main"
-            sx={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-            {error.status === 0 ? null : <AuthError />}
-            <Box sx={{width: '100%'}}>
-                {isLoading ?
-                    <CircularProgress color="primary" sx={{display:'block', margin:'auto'}}/> :
-                    <AuthForm mode='login'/>}
-            </Box>
-        </Container>
-    );
-};
+   return (
+      <Container
+         component="main"
+         sx={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+         }}
+      >
+         {error === undefined ? null : <AuthError error={error} />}
+         <Box sx={{ width: "100%" }}>
+            {isLoading ? (
+               <CircularProgress color="primary" sx={{ display: "block", margin: "auto" }} />
+            ) : (
+               <AuthForm mode="login" onLogin={loginUser} />
+            )}
+         </Box>
+      </Container>
+   )
+}
 
-export default LoginPage;
+export default LoginPage
